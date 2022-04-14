4e6cb6498d7bacbe79f6259bf8dad1c7b97beb71
David Galiev <galiev.david@yandex.ru>
2022-04-14 17:39:30 +0500

    Make hologramContentPrefab load only when requested

--- PlainHologram.cs
+++ PlainHologram.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 using TMPro;
 using R2API;
 using RoR2;
+using UnityEngine.AddressableAssets;
 
 namespace MysticsRisky2Utils
 {
     public static class PlainHologram
     {
-        public static GameObject hologramContentPrefab; // for use with IHologramContentProvider.GetHologramContentPrefab()
-
-        internal static void Init()
+        private static GameObject _hologramContentPrefab;
+        public static GameObject hologramContentPrefab // for use with IHologramContentProvider.GetHologramContentPrefab()
         {
-            hologramContentPrefab = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("Prefabs/CostHologramContent"), "MysticsRisky2UtilsPlainHologramContent", false);
-            CostHologramContent costHologramContent = hologramContentPrefab.GetComponent<CostHologramContent>();
-            MysticsRisky2UtilsPlainHologramContent plainHologramContent = hologramContentPrefab.AddComponent<MysticsRisky2UtilsPlainHologramContent>();
-            plainHologramContent.targetTextMesh = costHologramContent.targetTextMesh;
-            Object.Destroy(costHologramContent);
+            get
+            {
+                if (!_hologramContentPrefab)
+                {
+                    _hologramContentPrefab = PrefabAPI.InstantiateClone(Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/CostHologramContent.prefab").WaitForCompletion(), "MysticsRisky2UtilsPlainHologramContent", false);
+                    CostHologramContent costHologramContent = hologramContentPrefab.GetComponent<CostHologramContent>();
+                    MysticsRisky2UtilsPlainHologramContent plainHologramContent = hologramContentPrefab.AddComponent<MysticsRisky2UtilsPlainHologramContent>();
+                    plainHologramContent.targetTextMesh = costHologramContent.targetTextMesh;
+                    Object.Destroy(costHologramContent);
+                }
+                return _hologramContentPrefab;
+            }
         }
 
         public class MysticsRisky2UtilsPlainHologramContent : MonoBehaviour
