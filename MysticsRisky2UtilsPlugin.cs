f2cff739e20a6a034a0639bae5524c344caeea5f
David Galiev <galiev.david@yandex.ru>
2022-11-26 03:46:18 +0500

    Mark mod as client-side, add EliteAPI

--- MysticsRisky2UtilsPlugin.cs
+++ MysticsRisky2UtilsPlugin.cs
@@ -15,8 +15,8 @@ namespace MysticsRisky2Utils
     [BepInDependency(R2API.R2API.PluginGUID)]
     [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-    [R2APISubmoduleDependency(nameof(NetworkingAPI), nameof(LanguageAPI), nameof(PrefabAPI), nameof(RecalculateStatsAPI), nameof(SoundAPI))]
-    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(EliteAPI), nameof(NetworkingAPI), nameof(LanguageAPI), nameof(PrefabAPI), nameof(RecalculateStatsAPI), nameof(SoundAPI))]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     public class MysticsRisky2UtilsPlugin : BaseUnityPlugin
     {
         public const string PluginGUID = "com.themysticsword.mysticsrisky2utils";
