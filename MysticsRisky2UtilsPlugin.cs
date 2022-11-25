0ced72442c068e9ac172b3e868a42bdc4cf3a803
David Galiev <galiev.david@yandex.ru>
2022-11-26 03:46:27 +0500

    Update version

--- MysticsRisky2UtilsPlugin.cs
+++ MysticsRisky2UtilsPlugin.cs
@@ -21,7 +21,7 @@ namespace MysticsRisky2Utils
     {
         public const string PluginGUID = "com.themysticsword.mysticsrisky2utils";
         public const string PluginName = "MysticsRisky2Utils";
-        public const string PluginVersion = "2.0.6";
+        public const string PluginVersion = "2.0.8";
 
         internal static BepInEx.Logging.ManualLogSource logger;
         internal const BindingFlags bindingFlagAll = (BindingFlags)(-1);
